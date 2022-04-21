45f43f6a825871c1b4cbb08f6c39d8b6a9945207
Arshnaaz khan <arshnaazkhan123456@gmail.com>
2022-04-21 05:39:29 -0700

    ondelete

---
+++ on_deleteapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Auther(models.Model):  
+    first_name = models.CharField(max_length=30)  
+    last_name = models.CharField(max_length=30)  
+    email = models.EmailField()  
+  
+    def __str__(self):  
+        return "%s %s" % (self.first_name, self.last_name)  
+  
+class Post(models.Model):  
+    title = models.CharField(max_length=100)  
+    # Here we define the on_delete as CASCADE  
+    author = models.ForeignKey(Auther, on_delete=models.SET_NULL, null = True)  
+  
+    def __str__(self):  
+        return self.title 
